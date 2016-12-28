f279987decc4aef2e4135afa51178146b8befb16
Prelang Builder <builder@prelang.com>
2016-12-28 13:13:08 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Measure' to allow for create, update

--- app/admin/measure.rb
+++ app/admin/measure.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Measure do
+  permit_params :measurement, :type
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
